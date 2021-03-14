db28d1cc46126955b7bf7d55bec85dba4019e564
CybexGS <68516763+CybexGS@users.noreply.github.com>
2021-03-14 09:36:22 +0100

    Base Setup

--- DVSuperGauges/SuperGauges.cs
+++ DVSuperGauges/SuperGauges.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,71 @@ using UnityModManagerNet;
 
 namespace Cybex.DVSuperGauges
 {
+#if DEBUG
+	[EnableReloading]
+#endif
     public class SuperGauges
     {
+		public static bool enabled = true;
+		public static UnityModManager.ModEntry mod;
+		public static Settings settings = new Settings();
+
 		public static bool Load (UnityModManager.ModEntry modEntry)
 		{
+			mod = modEntry;
+			try { settings = Settings.Load<Settings>(modEntry); } catch { }
+
+			modEntry.OnGUI = OnGui;
+			modEntry.OnSaveGUI = OnSaveGui;
+			modEntry.OnToggle = OnToggle;
+
+#if DEBUG
+			modEntry.OnUnload = Unload;
+#endif
+
+			Log("Mod Entry Load Done!");
+
 			return true;
+		}
+
+		static bool OnToggle (UnityModManager.ModEntry modEntry, bool value)
+		{
+			if (value != enabled) enabled = value;
+			return true;
+		}
+
+		static void OnGui (UnityModManager.ModEntry modEntry)
+		{
+			settings.Draw(modEntry);
+		}
+
+		static void OnSaveGui (UnityModManager.ModEntry modEntry)
+		{
+			settings.Save(modEntry);
+		}
+
+#if DEBUG
+		static bool Unload (UnityModManager.ModEntry modEntry)
+		{
+			var harmony = new Harmony(modEntry.Info.Id);
+			harmony.UnpatchAll(modEntry.Info.Id);
+
+			return true;
+		}
+#endif
+
+		static void Log (string msg)
+		{
+			mod?.Logger.Log($"[DV Mouse Smoothing] {msg}");
+		}
+
+		public class Settings : UnityModManager.ModSettings, IDrawable
+		{
+			//[Draw("Hardness ", DrawType.Slider, Min = 1, Max = 10, Precision = 1)] public float dmpMul = 4;
+
+			override public void Save (UnityModManager.ModEntry entry) { Save<Settings>(this, entry); }
+
+			public void OnChange () { }
 		}
 	}
 }
