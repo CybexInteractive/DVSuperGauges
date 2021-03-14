ef3f5c08ad62e14c83ff740c6d869f1597040df7
CybexGS <68516763+CybexGS@users.noreply.github.com>
2021-03-14 09:01:10 +0100

    Add project files.

---
+++ DVSuperGauges/SuperGauges.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace Cybex.DVSuperGauges
+{
+    public class SuperGauges
+    {
+		public static bool Load (UnityModManager.ModEntry modEntry)
+		{
+			return true;
+		}
+	}
+}
